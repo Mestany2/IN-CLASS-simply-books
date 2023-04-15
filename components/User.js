46193c1e51f47477b7a00d7f16f11aa66257e807
Mestany2 <121625599+Mestany2@users.noreply.github.com>
2023-04-15 15:13:47 -0500

    author view

---
+++ components/User.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import { useAuth } from '../utils/context/authContext';
+import { signOut } from '../utils/auth';
+
+export default function User() {
+  const { user } = useAuth();
+  console.warn(user)
+
+  return (
+    <div className="text-light text-center">
+      <Image
+        src={user.photoURL}
+        alt="Profile"
+        width="100rm"
+        height="100rm"
+      />
+      <h1>{user.displayName}</h1>
+      <p>{user.email}</p>
+      <p>{user.metadata.lastSignInTime}</p>
+      <button type="button">{signOut}</button>
+    </div>
+  );
+}
---
+++ pages/author/[firebaseKey].js
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { viewAuthorDetails } from '../../api/mergedData';
+
+// inside component use
+export default function ViewAuthor() {
+  const [authorBooks, setAuthorBooks] = useState([]);
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    viewAuthorDetails(firebaseKey).then(setAuthorBooks);
+  }, [firebaseKey]);
+
+  return (
+    <div className="mt-5 d-flex flex-wrap">
+      <div className="d-flex flex-column">
+        <img src={authorBooks.image} alt={authorBooks.title} style={{ width: '300px' }} />
+      </div>
+      <div className="text-white ms-5 details">
+        <h5>
+          {authorBooks.title} by {authorBooks.authorObject?.first_name} {authorBooks.authorObject?.last_name}
+          {authorBooks.authorObject?.favorite ? ' 🤍' : ''}
+        </h5>
+        Author Email: <a href={`mailto:${authorBooks.authorObject?.email}`}>{authorBooks.authorObject?.email}</a>
+        <p>{authorBooks.description || ''}</p>
+        <hr />
+        <p>
+          {authorBooks.sale
+            ? `🏷️ Sale $${authorBooks.price}`
+            : `$${authorBooks.price}`}
+        </p>
+      </div>
+    </div>
+  );
+}
